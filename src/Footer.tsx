3b388b4615d7e1f79df0a2710e49014996df78df
Ruslan Oyun <careerRuslanOyun@yandex.ru>
2025-04-19 06:09:28 +0600

    fix

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-export const Footer: React.FC = () => (
+const Footer: React.FC = () => (
   <footer>
     <h1>Мой Footer из mfe-footer</h1>
   </footer>
 );
+export default Footer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Footer } from './Footer';
+import Footer from './Footer';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
